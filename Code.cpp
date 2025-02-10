e1060e20388010302ef10583fbe709c725b34ef9
Usama-Codez <usamaakram442@gmail.com>
2025-02-10 23:29:29 +0500

    GCD Code updated

--- Code.cpp
+++ Code.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int func(int sum){
-    return sum;
+// Function to calculate GCD using Euclidean algorithm
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
 
-int main () {
+int main() {
     int num1, num2;
-    cout << func(num1+num2);
     
+    // Taking user input
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    
+    // Calculating and displaying the GCD
+    cout << "GCD of " << num1 << " and " << num2 << " is: " << gcd(num1, num2) << endl;
+
     return 0;
 }
