9e691bde2d4b8303dde084027c4c7ac0d1e23939
Usama-Codez <usamaakram442@gmail.com>
2025-02-14 23:28:27 +0500

    Updated Code

--- Code.cpp
+++ Code.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <unistd.h>  
+#include <unistd.h>   // For usleep() on Linux/Mac
 #include <windows.h>  // For Sleep(), system("CLS"), and PlaySound on Windows
 #include <mmsystem.h> // Windows library for playing sound (Link with -lwinmm)
 using namespace std;
